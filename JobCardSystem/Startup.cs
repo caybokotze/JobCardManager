a9174d2d92f2917de777233aad4193fe62a49b7a
21647804 <21647804@dut4life.ac.za>
2019-06-13 15:55:18 +0200

    Add project files.

---
+++ JobCardSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobCardSystem.Startup))]
+namespace JobCardSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
